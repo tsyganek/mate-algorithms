79bd1d9825f2b07bfd15c00fcff458647a3c6c8e
Kateryna Tsygankova <kateryna.tsygankova.se@gmail.com>
2022-11-09 11:15:39 +0200

    Assignment 3-4 solved

--- 3-4.js
+++ 3-4.js
@@ -3,15 +3,33 @@
 // Return the running sum of nums.
 
 function runningSum(nums) {
-  let sumArray = [];
-  let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
-    sumArray.push(sum);
+  let el;
+  //   let sumArray = [];
+  //   let sum = 0;
+  //   for (let i = 0; i < nums.length; i++) {
+  //     sum += nums[i];
+  //     sumArray.push(sum);
+  //   }
+  //   return sumArray;
+  if (nums.length > 2) {
+    for (let i = 0; i <= Math.floor((nums.length - 1) / 2); i++) {
+      el = nums[i];
+      nums[i] = nums[nums.length - 1 - i];
+      nums[nums.length - 1 - i] = el;
+    }
+  } else if ((nums.length = 2)) {
+    el = nums[0];
+    nums[0] = nums[1];
+    nums[1] = el;
   }
-  return sumArray;
+  console.log(nums);
+  return nums;
 }
 
 runningSum([1, 2, 3, 4]);
 runningSum([1, 1, 1, 1, 1]);
 runningSum([3, 1, 2, 10, 1]);
+runningSum([1, 2, 3]);
+runningSum([1, 2]);
+
+runningSum([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
